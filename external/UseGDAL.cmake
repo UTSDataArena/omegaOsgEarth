ab2e01d1c5fcadb471739fedb502bdfcf91a56fe
mvictoras <mvictoras@gmail.com>
2013-11-14 17:59:09 -0600

    Latest Gdal file

--- external/UseGDAL.cmake
+++ external/UseGDAL.cmake
@@ -2,21 +2,33 @@
 set(NETCDF netcdf-4.3.0)
 set(NETCDF_TGZ ${NETCDF}.tar.gz)
 
-if(NOT EXISTS ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF_TGZ})
-  file(DOWNLOAD ftp://ftp.unidata.ucar.edu/pub/netcdf/${NETCDF_TGZ} ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF_TGZ} SHOW_PROGRESS)
-endif()
 
-if(NOT EXISTS ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF})
-  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzvf ${NETCDF_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth)
-endif()
+ExternalProject_Add(
+  netcdf
+  URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/${NETCDF_TGZ}"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(netcdf SOURCE_DIR)
+
+  #if(NOT EXISTS ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF_TGZ})
+  #file(DOWNLOAD ftp://ftp.unidata.ucar.edu/pub/netcdf/${NETCDF_TGZ} ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF_TGZ} SHOW_PROGRESS)
+  #endif()
+
+  #if(NOT EXISTS ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF})
+  #execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzvf ${NETCDF_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/modules/omegaOsgEarth)
+  #endif()
 
 ExternalProject_Add(
     gdal
+    DEPENDS netcdf
     URL "http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz"
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure --with-netcdf=${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/${NETCDF} --prefix=${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/gdal-prefix/src/gdal-install
+    CONFIGURE_COMMAND <SOURCE_DIR>/configure --with-netcdf=${SOURCE_DIR} --prefix=${CMAKE_BINARY_DIR}/modules/omegaOsgEarth/gdal-prefix/src/gdal-install
     BUILD_IN_SOURCE 1
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
+    #    BUILD_COMMAND make
+    INSTALL_COMMAND ${PLATFORM_INSTALL_COMMAND}
 )
 
 ExternalProject_Get_Property(gdal DOWNLOAD_DIR)
